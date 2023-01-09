0916888c40ec893d2a79e80d39ea45e8cf36f0a9
rahneverd <rahneverd@gmail.com>
2023-01-09 12:05:04 +0500

    home page component design completed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+   firebaseConfig : {
+    apiKey: "AIzaSyDm-K-4-ZKwUuRmslSgPxtv50iddx_xBp4",
+    authDomain: "ngsocialmediaapp.firebaseapp.com",
+    projectId: "ngsocialmediaapp",
+    storageBucket: "ngsocialmediaapp.appspot.com",
+    messagingSenderId: "397171532169",
+    appId: "1:397171532169:web:df7be09a92300ef47cc158",
+    measurementId: "G-5H87V8N32K"
+    },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig : {
+  apiKey: "AIzaSyDm-K-4-ZKwUuRmslSgPxtv50iddx_xBp4",
+  authDomain: "ngsocialmediaapp.firebaseapp.com",
+  projectId: "ngsocialmediaapp",
+  storageBucket: "ngsocialmediaapp.appspot.com",
+  messagingSenderId: "397171532169",
+  appId: "1:397171532169:web:df7be09a92300ef47cc158",
+  measurementId: "G-5H87V8N32K"
+  },
 };
 
 /*
