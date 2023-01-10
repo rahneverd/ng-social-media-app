53825876270c9074a8ebe8f4714b707b09b23e4e
rahneverd <rahneverd@gmail.com>
2023-01-10 19:33:15 +0500

    login logout feature added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,30 @@
 import { Component } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { AuthenticatorComponent } from './tools/authenticator/authenticator.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-social-media-app';
-  constructor(private loginSheet: MatBottomSheet) {}
+  isLoggedIn = false;
+  constructor(private loginSheet: MatBottomSheet) {
+    onAuthStateChanged(getAuth(), (user) => {
+      if (user) {
+        this.isLoggedIn = true;
+      } else {
+        this.isLoggedIn = false;
+      }
+    });
+  }
   onLoginClick() {
-    this.loginSheet.open(AuthenticatorComponent)
+    this.loginSheet.open(AuthenticatorComponent);
+  }
+  onLogoutClick() {
+    getAuth().signOut();
+    // this.isLoggedIn = false;
   }
 }
--- src/app/tools/authenticator/authenticator.component.ts
+++ src/app/tools/authenticator/authenticator.component.ts
@@ -1,53 +1,64 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { Validators } from '@angular/forms';
-import { MatBottomSheetRef } from '@angular/material/bottom-sheet'
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
 //Firebase
-import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendPasswordResetEmail, getAdditionalUserInfo} from 'firebase/auth';
-
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
 
 @Component({
   selector: 'app-authenticator',
   templateUrl: './authenticator.component.html',
-  styleUrls: ['./authenticator.component.css']
+  styleUrls: ['./authenticator.component.css'],
 })
 export class AuthenticatorComponent implements OnInit {
-  state = "LOGIN"
-  emailAlreadyInUse = false
-  userNotFound = false
-  wrongPassword = false
-  invalidEmail = false
-  // firebasetsAuth: FirebaseTSAuth
-  constructor(private _fb: FormBuilder, private bottomSheetRef: MatBottomSheetRef) {
-    // this.firebasetsAuth = new FirebaseTSAuth()
-  }
+  state = 'LOGIN';
+  emailAlreadyInUse = false;
+  userNotFound = false;
+  wrongPassword = false;
+  invalidEmail = false;
+  constructor(
+    private _fb: FormBuilder,
+    private bottomSheetRef: MatBottomSheetRef
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   regForm = this._fb.group({
     email: ['', [Validators.email, Validators.required]],
-    password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]],
-    confirmPassword: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]]
-  })
+    password: [
+      '',
+      [Validators.required, Validators.minLength(8), Validators.maxLength(16)],
+    ],
+    confirmPassword: [
+      '',
+      [Validators.required, Validators.minLength(8), Validators.maxLength(16)],
+    ],
+  });
 
   loginForm = this._fb.group({
     email: ['', [Validators.email, Validators.required]],
-    password: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(16)]]
-  })
+    password: [
+      '',
+      [Validators.required, Validators.minLength(8), Validators.maxLength(16)],
+    ],
+  });
 
   resetForm = this._fb.group({
-    email: ['', [Validators.email, Validators.required]]
-  })
+    email: ['', [Validators.email, Validators.required]],
+  });
 
   onForgotPasswordClick() {
-    this.state = "FORGOT"
+    this.state = 'FORGOT';
   }
-  onCreateClick(){
-    this.state = "REGISTER"
+  onCreateClick() {
+    this.state = 'REGISTER';
   }
   onLoginClick() {
-    this.state = "LOGIN"
+    this.state = 'LOGIN';
   }
 
   onRegSubClick() {
@@ -57,14 +68,16 @@ export class AuthenticatorComponent implements OnInit {
       this.regForm.controls.email.value
     )
       .then((user) => {
-        console.log("User signed in")
-        console.log(user)
-        this.bottomSheetRef.dismiss()
+        console.log('User signed in');
+        console.log(user);
+        this.bottomSheetRef.dismiss();
       })
       .catch((err) => {
-        err.code == "auth/email-already-in-use" ? this.emailAlreadyInUse = true : this.emailAlreadyInUse = false
-        console.log(this.emailAlreadyInUse)
-      })
+        err.code == 'auth/email-already-in-use'
+          ? (this.emailAlreadyInUse = true)
+          : (this.emailAlreadyInUse = false);
+        console.log(this.emailAlreadyInUse);
+      });
   }
   onLoginSubClick() {
     signInWithEmailAndPassword(
@@ -73,31 +86,38 @@ export class AuthenticatorComponent implements OnInit {
       this.loginForm.controls.password.value
     )
       .then((data) => {
-        console.log(data)
-        this.bottomSheetRef.dismiss()
+        console.log(data);
+        console.log(data.user.emailVerified);
+        this.bottomSheetRef.dismiss();
       })
       .catch((err) => {
-        err.code == "auth/user-not-found" ? this.userNotFound = true : this.userNotFound = false
-        err.code == "auth/wrong-password" ? this.wrongPassword = true : this.wrongPassword = false
-      })
+        err.code == 'auth/user-not-found'
+          ? (this.userNotFound = true)
+          : (this.userNotFound = false);
+        err.code == 'auth/wrong-password'
+          ? (this.wrongPassword = true)
+          : (this.wrongPassword = false);
+      });
   }
   onResetSubClick() {
     sendPasswordResetEmail(getAuth(), this.resetForm.controls.email.value)
       .then((data) => {
-        console.log("sent")
-        console.log(data)
-        this.bottomSheetRef.dismiss()
+        console.log('sent');
+        console.log(data);
+        this.bottomSheetRef.dismiss();
       })
       .catch((err) => {
-        console.log("Error")
-        console.log(err)
-        err.code == "auth/invalid-email" ? this.invalidEmail = true : this.invalidEmail = false
-      })
+        console.log('Error');
+        console.log(err);
+        err.code == 'auth/invalid-email'
+          ? (this.invalidEmail = true)
+          : (this.invalidEmail = false);
+      });
   }
 }
 
 export enum AuthenticatorCompStat {
   LOGIN,
   REGISTER,
-  FORGOT_PASSWORD
+  FORGOT_PASSWORD,
 }
